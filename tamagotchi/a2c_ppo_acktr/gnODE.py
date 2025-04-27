28c2bbd4c910acb95df1e6e47e65335411a50445
lolrenceH <lawrenhu@bu.edu>
2025-04-27 00:59:13 +0000

    debug gnODE need the seq handling for evaluate action function

--- tamagotchi/a2c_ppo_acktr/gnODE.py
+++ tamagotchi/a2c_ppo_acktr/gnODE.py
@@ -17,16 +17,30 @@ class GNODE(nn.Module):
         self.W_2 = nn.Linear(layer_size, hidden_size)
 
     def forward(self, x, h_prev):
+        '''
+        x: input tensor of shape [seq_len, batch_size, input_size]
+        h_prev: previous hidden state of shape [1, batch_size, hidden_size]
+        '''
+        seq_len, batch_size, _ = x.size()
+        h_seq = [] 
         h_t = h_prev
-        combined = torch.cat((x, h_t), dim=2)
-        # Update gate
-        z_t = torch.sigmoid(self.W_zr(combined))
-        # Candidate hidden state
-        combined_r = torch.cat((x, h_t), dim=2)
-        h_candidate = F.relu(self.W_1(combined_r))
-        h_candidate = F.relu(self.W_2(h_candidate))
-        h_candidate = F.tanh(h_candidate)
-        # Updated hidden state
-        h_t = (1 - z_t) * h_t + z_t * h_candidate
+
+        for t in range(seq_len):
+            x_t = x[t:t+1, :, :]
+            # Concatenate input and previous hidden state
+            combined = torch.cat((x_t, h_t), dim=-1)
+
+            # Update gate
+            z_t = torch.sigmoid(self.W_zr(combined))
+            # Candidate hidden state
+            combined_r = torch.cat((x_t, h_t), dim=-1)
+            h_candidate = F.relu(self.W_1(combined_r))
+            h_candidate = F.relu(self.W_2(h_candidate))
+            h_candidate = F.tanh(h_candidate)
+            # Updated hidden state
+            h_t = (1 - z_t) * h_t + z_t * h_candidate
+            h_seq.append(h_t)
+
+        h_seq = torch.stack(h_seq, dim=0).squeeze(1) # shape [seq_len, batch_size, hidden_size]
         
-        return h_t, h_t # match output structure of VRNN
+        return h_seq, h_t
